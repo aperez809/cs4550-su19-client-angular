b13d2ca78ffdaa0110d45bbc201117c8bacf2f2b
aperez809 <32823645+aperez809@users.noreply.github.com>
2019-06-25 18:14:54 -0400

    implemented client side PageService

--- src/app/services/PageService.ts
+++ src/app/services/PageService.ts
@@ -1,10 +1,11 @@
 import {Injectable} from '@angular/core';
+import Page from "../../models/Page";
 
 @Injectable()
 export default class PageService {
   PAGES_URL = 'http://localhost:3001/api/websites/WID/pages';
 
-  createPage = (wid, page) =>
+  createPage = (wid: string, page: Page) =>
     fetch(this.PAGES_URL.replace('WID', wid), {
       method: 'post',
       body: JSON.stringify(page),
@@ -14,17 +15,28 @@ export default class PageService {
     })
       .then(response => response.json());
 
-  findPagesForWebsite = wid =>
-    fetch(this.PAGES_URL.replace('WID', wid))
+  findAllPagesForWebsite(wid: string) {
+    return fetch(this.PAGES_URL.replace('WID', wid))
       .then(response => response.json());
+  }
 
-  findPageById = (wid, pid) =>
-    fetch(this.PAGES_URL.replace('WID', wid) + '/' + pid)
+  findPageById(wid: string, pid: string) {
+    return fetch(this.PAGES_URL.replace('WID', wid) + '/' + pid)
       .then(response => response.json());
-
+  }
   updatePage(websiteId: string, pageId: string, page: { _id: string; title: string; rows: any[] }) {
-    fetch(this.PAGES_URL.replace('WID', websiteId) + '/' + pageId, {
+    return fetch(this.PAGES_URL.replace('WID', websiteId) + '/' + pageId, {
       method: 'PUT',
+      body: JSON.stringify(page),
+      headers: {
+        'content-type': 'application/json'
+      }
+    }).then(response => response.json());
+  }
+
+  deletePage(websiteId: string, pageId: string) {
+    return fetch(this.PAGES_URL.replace('WID', websiteId) + '/' + pageId, {
+      method: 'DELETE',
       body: JSON.stringify(page),
       headers: {
         'content-type': 'application/json'
